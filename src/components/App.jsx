de45313aa6128953eeda0ab7c4ff7d9c45510455
Yurii <gapirov21@gmail.com>
2023-10-30 20:28:41 +0200

    task2 start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,13 @@
 import user from '../data/user.json';
+import data from '../data/data.json';
+
 
 import { Profile } from './Profile/Profile';
+import { Statistic } from './Statistics/Statistics';
+
 
 export const App = () => {
-  return <Profile user={user} />;
+   return <Profile user={user} />,
+  <Statistic title="Upload stats" data={data} />,
+  <Statistic data={data} />;
 };
---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,7 @@
+export const Statistic = ({ title, stats }) => {
+    return (
+        <section>
+            <h2>{title}</h2>
+        </section>
+    )
+}
